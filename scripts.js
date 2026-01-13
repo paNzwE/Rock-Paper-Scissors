2811cf49ed0816391818b611d4328cfffcfd88ff
paNzwE <166597701+paNzwE@users.noreply.github.com>
2026-01-13 17:54:28 +0200

    make the functions for computer and human choices

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+const random = Math.floor(Math.random() * 3) + 1;
+let randomChoice ;
+if (random === 1) {
+   randomChoice = "rock" ;
+} else if (random === 2){
+   randomChoice = "paper";
+} else {
+    randomChoice =  "scissors";
+}
+return randomChoice;
+}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+let personChoice = prompt("Please enter your choice!", "")
+return personChoice;
+
+}
+console.log(getHumanChoice());
+
+
+
+
